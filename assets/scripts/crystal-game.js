e2d7b0f7495caadebfadf588fc1cd14d07b415a2
Kit Aronoff <dreamnmusic@gmail.com>
2019-04-01 00:15:27 -0400

    Starting trying to figure out how to program the reset button. I'm not sure why it isn't grabbing the id in the HTML.

--- assets/scripts/crystal-game.js
+++ assets/scripts/crystal-game.js
@@ -6,13 +6,17 @@ $(document).ready(function(){
   var imageNum = [];
   var totalScore;
   
+  var $image0 = $("#image0");
+  var $image1 = $("#image1");
+  var $image2 = $("#image2");
+  var $image3 = $("#image3");
 
 // functions for the start game button
   $("#buttonStart").click(function(){
     imageNum = [];
     // generating the numbers for the cat images
     for (i=0; i < 4; i++){
-      var ranNum = Math.floor(Math.random()*12);
+      var ranNum = Math.floor(Math.random()*11+1);
       imageNum.push(ranNum);
 
     }
@@ -45,19 +49,72 @@ $(document).ready(function(){
     $resetBtn.text("Reset Game");
     $("#startGameArea").append($resetBtn);
 
-    $("#image0").attr("data-num", imageNum[0]);
-    $("#image1").attr("data-num", imageNum[1]);
-    $("#image2").attr("data-num", imageNum[2]);
-    $("#image3").attr("data-num", imageNum[3]);
 
-    var catNum = parseInt;
-    console.log(`this is catNum ${catNum}`)
+
+    $image0.attr("data-num", imageNum[0]);
+    $image1.attr("data-num", imageNum[1]);
+    $image2.attr("data-num", imageNum[2]);
+    $image3.attr("data-num", imageNum[3]);
+
   })
+// reset the game by pushing the reset button
+  $("#resetBtn").click(function(){
+    
+    wins = 0;
+    losses = 0;
+    currentScore = 0;
+    totalScore = 0;
+    targetScore = Math.floor(Math.random()*101+19)
+    console.log(imageNum)
+    console.log(`the target score generated ${targetScore}`)
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#totalScore").text(totalScore);
+    $("#currentScore").text(currentScore);
+    $("#targetScore").text(targetScore);
 
+    imageNum = [];
+    // generating the numbers for the cat images
+    for (i=0; i < 4; i++){
+      var ranNum = Math.floor(Math.random()*11+1);
+      imageNum.push(ranNum);
+    }
+
+  });
 // game play
+// cat images add to to the current score
+  $image0.click(function(){
+    var cat0 = parseInt($(this).attr("data-num"));
+    console.log(`this is cat0 ${cat0}`);
 
-  $("#image0").click(function(){
-    currentScore = currentScore + catA;
+    currentScore = cat0 + currentScore;
     $("#currentScore").text(currentScore);
   });
+
+  $image1.click(function(){
+    var cat1 = parseInt($(this).attr("data-num"));
+    console.log(`this is cat1 ${cat1}`);
+
+    currentScore = cat1 + currentScore;
+    $("#currentScore").text(currentScore);
+  });
+
+  $image2.click(function(){
+    var cat2 = parseInt($(this).attr("data-num"));
+    console.log(`this is cat2 ${cat2}`);
+
+    currentScore = cat2 + currentScore;
+    $("#currentScore").text(currentScore);
+  });
+
+  $image3.click(function(){
+    var cat3 = parseInt($(this).attr("data-num"));
+    console.log(`this is cat3 ${cat3}`);
+
+    currentScore = cat3 + currentScore;
+    $("#currentScore").text(currentScore);
+  });
+
+  
+
 })
