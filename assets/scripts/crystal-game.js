8526be781be164f2e23871b0ba7c506cfde228b0
Kit Aronoff <dreamnmusic@gmail.com>
2019-04-02 09:40:51 -0400

    Added some logic gates so you can't continue to play the game after the game endds without pressing the restart or new round buttons

--- assets/scripts/crystal-game.js
+++ assets/scripts/crystal-game.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
   var $image1 = $("#image1");
   var $image2 = $("#image2");
   var $image3 = $("#image3");
+  var gamePlay = false
 
 // functions for the start game button
   $("#buttonStart").click(function(){
@@ -26,6 +27,7 @@ $(document).ready(function(){
     currentScore = 0;
     totalScore = 0;
     targetScore = Math.floor(Math.random()*101+19)
+    gamePlay = true;
     console.log(imageNum)
     console.log(`the target score generated ${targetScore}`)
     $("#wins").text(wins);
@@ -65,6 +67,7 @@ $(document).ready(function(){
     currentScore = 0;
     totalScore = 0;
     targetScore = Math.floor(Math.random()*101+19)
+    gamePlay = true;
     console.log(imageNum)
     console.log(`the target score generated ${targetScore}`)
     $("#wins").text(wins);
@@ -85,7 +88,7 @@ $(document).ready(function(){
   $(document).on("click", "#newRoundBtn", function(){
     currentScore = 0;
     targetScore = Math.floor(Math.random()*101+19)
-
+    gamePlay = true;
     $("#currentScore").text(currentScore);
     $("#targetScore").text(targetScore);
 
@@ -99,6 +102,10 @@ $(document).ready(function(){
 // game play
 // cat images add to to the current score
   $image0.click(function(){
+
+    if (gamePlay === false) {
+      return false;
+    }
     var cat0 = parseInt($(this).attr("data-num"));
     console.log(`this is cat0 ${cat0}`);
 
@@ -111,16 +118,23 @@ $(document).ready(function(){
       wins = wins + 1;
       $("#currentScore").text(currentScore);
       $("#wins").text(wins);
+      gamePlay = false;
     }
     else if (currentScore > targetScore){
       alert("You lose :(");
       losses = losses + 1;
       $("#losses").text(losses);
+      gamePlay = false;
       
     }
   });
 
   $image1.click(function(){
+
+    if (gamePlay === false) {
+      return false;
+    }
+
     var cat1 = parseInt($(this).attr("data-num"));
     console.log(`this is cat1 ${cat1}`);
 
@@ -135,16 +149,22 @@ $(document).ready(function(){
       wins = wins + 1;
       $("#currentScore").text(currentScore);
       $("#wins").text(wins);
+      gamePlay = false;
     }
     else if (currentScore > targetScore){
       alert("You lose :(");
       losses = losses + 1;
       $("#losses").text(losses);
+      gamePlay = false;
       
     }
   });
 
   $image2.click(function(){
+
+    if (gamePlay === false) {
+      return false;
+    }
     var cat2 = parseInt($(this).attr("data-num"));
     console.log(`this is cat2 ${cat2}`);
 
@@ -159,16 +179,23 @@ $(document).ready(function(){
       wins = wins + 1;
       $("#currentScore").text(currentScore);
       $("#wins").text(wins);
+      gamePlay = false;
     }
     else if (currentScore > targetScore){
       alert("You lose :(");
       losses = losses + 1;
       $("#losses").text(losses);
+      gamePlay = false;
       
     }
   });
 
   $image3.click(function(){
+
+    if (gamePlay === false) {
+      return false;
+    }
+
     var cat3 = parseInt($(this).attr("data-num"));
     console.log(`this is cat3 ${cat3}`);
 
@@ -183,11 +210,13 @@ $(document).ready(function(){
     wins = wins + 1;
     $("#currentScore").text(currentScore);
     $("#wins").text(wins);
+    gamePlay = false;
   }
   else if (currentScore > targetScore){
     alert("You lose :(");
     losses = losses + 1;
     $("#losses").text(losses);
+    gamePlay = false;
     
   }
   });
